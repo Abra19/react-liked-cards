2ac98a22053b149fefbb641d7f57895350569aa6
elena_abrarova <evabrarova@gmail.com>
2022-12-15 05:08:20 +0300

    make layout

--- app/src/routes.js
+++ app/src/routes.js
@@ -1,6 +1,6 @@
 const paths =  {
   textPath: () => "https://meowfacts.herokuapp.com/?count=48",
-  imgPath: () => "http://shibe.online/api/cats?count=48",
+  imgPath: () => "https://shibe.online/api/cats?count=48",
 };
 
 export default paths;
